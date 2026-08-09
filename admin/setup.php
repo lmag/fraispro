9a4ee02c0386f9acedd157811864130d62dabfad
Laurent Magnin <laurent1magnin@gmail.com>
2026-08-09 08:20:36 +0200

    fix(fraispro): resolve case mismatch for FraisproReceipt in setup.php causing crash and hack attempt errors

--- admin/setup.php
+++ admin/setup.php
@@ -137,6 +137,7 @@ $myTmpObjects = array();
 $myTmpObjects['FraisproReceipt'] = array('label' => 'FraisproReceipt', 'includerefgeneration' => 1, 'includedocgeneration' => 0, 'class' => 'FraisproReceipt');
 
 $tmpobjectkey = GETPOST('object', 'aZ09');
+if ($tmpobjectkey == 'fraisproreceipt') $tmpobjectkey = 'FraisproReceipt'; // Fix case issue from strtolower in links
 if ($tmpobjectkey && !array_key_exists($tmpobjectkey, $myTmpObjects)) {
 	accessforbidden('Bad value for object. Hack attempt ?');
 }
@@ -336,7 +337,9 @@ foreach ($myTmpObjects as $myTmpObjectKey => $myTmpObjectArray) {
 							}
 
 							if ($module->isEnabled()) {
-								dol_include_once('/'.$moduledir.'/class/'.strtolower($myTmpObjectKey).'.class.php');
+								$classname_for_include = strtolower($myTmpObjectKey);
+								if ($classname_for_include == 'fraisproreceipt') $classname_for_include = 'fraispro_receipt';
+								dol_include_once('/'.$moduledir.'/class/'.$classname_for_include.'.class.php');
 
 								print '<tr class="oddeven"><td>'.$module->getName($langs)."</td><td>\n";
 								print $module->info($langs);
