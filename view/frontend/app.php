d5645d45295c3594292b07306f654cee92d8073f
Laurent Magnin <laurent1magnin@gmail.com>
2026-08-09 18:51:07 +0200

    feat(fraispro): display photo filename instead of audio filename in receipt title

--- view/frontend/app.php
+++ view/frontend/app.php
@@ -366,21 +366,25 @@ if ($resql) {
             $fileTitleInfo = '';
             
             if (dol_is_dir($dir)) {
+                $imageFiles = dol_dir_list($dir, 'files', 0, '\.(png|jpg|jpeg|gif|webp)$', '(?i)\.meta$', 'date', SORT_DESC);
                 $allFiles = dol_dir_list($dir, 'files', 0, '', '(?i)\.meta$', 'date', SORT_DESC);
-                if (!empty($allFiles)) {
-                    $firstFileObj = $allFiles[0];
-                    $firstFile = $firstFileObj['name'];
-                    $fileSize = isset($firstFileObj['size']) ? (int)$firstFileObj['size'] : 0;
+                
+                $titleFileObj = null;
+                if (!empty($imageFiles)) {
+                    $titleFileObj = $imageFiles[0];
+                    $thumbUrl = DOL_URL_ROOT . '/document.php?modulepart=fraispro&entity=1&file=' . urlencode(dol_sanitizeFileName($ref) . '/' . $titleFileObj['name']);
+                } elseif (!empty($allFiles)) {
+                    $titleFileObj = $allFiles[0];
+                }
+                
+                if ($titleFileObj) {
+                    $firstFile = $titleFileObj['name'];
+                    $fileSize = isset($titleFileObj['size']) ? (int)$titleFileObj['size'] : 0;
                     if ($fileSize == 0 && file_exists($dir . '/' . $firstFile)) {
                         $fileSize = (int)filesize($dir . '/' . $firstFile);
                     }
                     $fileSizeKo = round($fileSize / 1024);
                     $fileTitleInfo = ' - ' . $firstFile . ' (' . $fileSizeKo . 'ko)';
-                }
-                
-                $imageFiles = dol_dir_list($dir, 'files', 0, '\.(png|jpg|jpeg|gif|webp)$', '(?i)\.meta$', 'date', SORT_DESC);
-                if (!empty($imageFiles)) {
-                    $thumbUrl = DOL_URL_ROOT . '/document.php?modulepart=fraispro&entity=1&file=' . urlencode(dol_sanitizeFileName($ref) . '/' . $imageFiles[0]['name']);
                 }
             }
             
